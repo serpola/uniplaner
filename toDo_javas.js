329bffb0f42741fb59ffaa2691a560008dcb6ad8
rinafunk <rina.funk@stud.fh-rosenheim.de>
2017-12-19 15:59:04 +0100

    An Javascript rumprobiert um neue Tabellenzeile per Button hinzu zufügen

--- toDo_javas.js
+++ toDo_javas.js
@@ -1,4 +1,16 @@
-document.getElementById("#add").onclick = function() {add();};
+/*document.getElementById('#add').onclick = function() {add();};
 
 function add() {  
-}
+}*/
+
+$(document).ready(function(){
+  
+    $("#add").click(function (){
+        
+        $(".liste").append(
+        "<tr>" + /*Inputfeld und checkbox*/
+        "</tr>");
+        
+    })
+})
+
