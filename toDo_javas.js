c70419b15804b886bcd84da73913ee363137bf6d
rinafunk <rina.funk@stud.fh-rosenheim.de>
2017-12-21 13:37:26 +0100

    Tabelle bearbeitet

--- toDo_javas.js
+++ toDo_javas.js
@@ -1,11 +1,14 @@
 
-
-function myFunction() {
-    
+function add() {
+var n;
    var table =  document.getElementById('liste');
-   
-    var row = table.insertRow(0);
+
+    var row = table.insertRow(n);
     var cell1 = row.insertCell(0);
-    cell1.innerHTML = "<input type="checkbox">
-                        <input type="text">";
+    cell1.innerHTML += '<input type="checkbox"> <input type="text">';
+    n = n+1;
 }    
+
+function entfernen() {
+ document.getElementById('liste').deleteRow(n);
+}
