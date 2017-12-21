e9212cbc96d795998d03e15d87b81f18957b2b44
rinafunk <rina.funk@stud.fh-rosenheim.de>
2017-12-21 12:44:36 +0100

    Problem behoben

--- toDo_javas.js
+++ toDo_javas.js
@@ -6,7 +6,6 @@ function myFunction() {
    
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
+    cell1.innerHTML = "<input type="checkbox">
+                        <input type="text">";
 }    
