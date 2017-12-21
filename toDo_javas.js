16de670accf83b1abb55d63247f83c261f4f4c03
rinafunk <rina.funk@stud.fh-rosenheim.de>
2017-12-21 11:49:13 +0100

    Versucht Problem zu beheben

--- toDo_javas.js
+++ toDo_javas.js
@@ -1,13 +1,12 @@
-var document = typeof document === 'undefined' ? '' : document;
 
-(function(){
-    
+
 function myFunction() {
-    var table = document.getElementById("liste");
+    
+   var table =  document.getElementById('liste');
+   
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     cell1.innerHTML = "NEW CELL1";
     cell2.innerHTML = "NEW CELL2";
 }    
-})
