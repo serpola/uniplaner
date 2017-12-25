805456e7c04d9d6f6ddb4c715223499ec1413f6e
Ella <ella.kallmuenzer@stud.fh-rosenheim.de>
2017-12-25 10:01:53 +0100

    Vue

--- static/javascript/noten_java.js
+++ static/javascript/noten_java.js
@@ -1,24 +1,14 @@
-function newTableRow() { //erster Verusch zum einfügen einer Tabellenzeile
-	var table_id = "tables";
-	var table = document.getElementById('table_id');
-	var row = table.getElementsByTagName('tr').length;
-	var tr = table.insertRow( rows );
-	var td1 = document.createElement("td");
-	var td2 = document.createElement("td");
-	var td3 = document.createElement("td");
-	var td4 = document.createElement("td");
-	td1.innerHTML = '<input type="text" name="modul">'
-	td2.innerHTML = '<input type="text" name="note">'
-	td3.innerHTML = '<input type="text" name="ects">'
-	td4.innerHTML = '<input type="text" name="datum">'
-	tr.appendChild( td1);
-	tr.appendChild( td2);
-	tr.appendChild( td3);
-	tr.appendChild( td4);
-} //noch nciht getestet
-function AverageGrades() {
 
-}
-function CalculatorECTS() {
-
-}
+new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello World!'
+	}
+});
+new Vue({
+	el: '#tables',
+	data: {
+		blabla: 'Hello World!'
+	}
+}) // nun sollten in allen Inputfelder Hello World drin stehen: v-model
+// funktioniert aber noch nicht
