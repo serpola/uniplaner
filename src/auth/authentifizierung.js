95692489c6ed29f628395c340488a42cf9c0a137
DESKTOP-UCRR9SV\Polat <polat.s@live.de>
2018-01-04 12:39:02 +0100

    auth

---
+++ src/auth/authentifizierung.js
@@ -0,0 +1,52 @@
+import {router} from '../main';
+
+var api_url = 'http://localhost:8080';
+var login_url= api_url+'/api/authenticate'
+var signup_url = '/api/register';
+
+export default {
+    user:{
+        authenticated: false
+    },
+
+    login(context, creds, redirect){
+        context.axios.post(login_url, creds, (data)=>{
+            localStorage.setItem('token', data.id_token)
+            this.user.authenticated = true
+            if(redirect){
+                router.go(redirect)
+            }
+        }).then((response)=>{context.$router.push({name:'Start'})})
+
+    },
+
+    signup(context,creds, redirect){
+        context.axios.post(login_url, creds, (data)=>{
+            localStorage.setItem('token', data.id_token)
+            this.user.authenticated = true
+            if(redirect){
+                router.go(redirect)
+            }
+        }).then((response)=>{context.$router.push({name:'Start'})})
+    },
+//.then((response)=>{this.$router.push({name:'login'})})
+    logout(){
+        localStorage.removeItem('id_token')
+        this.user.authenticated=false
+    },
+
+    checkAuth(){
+        var jwt = localStorage.getItem('id_token')
+        if(jwt){
+            this.user.authenticated=true
+        }else{
+            this.user.authenticated=false
+        }
+    },
+
+    getAuthHeader(){
+        return{
+            'Authorization':'Bearer'+localStorage.getItem('id_token')
+        }
+    }
+}
