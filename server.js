3bd6523684ab8e254c7d68da61f97e46e1b28e2d
DESKTOP-UCRR9SV\Polat <polat.s@live.de>
2018-01-18 13:38:59 +0100

    Kalender events pust und getted

--- server.js
+++ server.js
@@ -101,43 +101,6 @@ apiRoutes.post('/authenticate', function(req, res) {
     });
 });
 
-
-
-
-// ---------------------------------------------------------
-// route middleware to authenticate and check token
-// ---------------------------------------------------------
-/*apiRoutes.use(function(req, res, next) {
-
-    // check header or url parameters or post parameters for token
-    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
-
-    // decode token
-    if (token) {
-
-        // verifies secret and checks exp
-        jwt.verify(token, app.get('Secret'), function(err, decoded) {
-            if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                next();
-            }
-        });
-
-    } else {
-
-        // if there is no token
-        // return an error
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
-
-    }
-
-});*/
 apiRoutes.use(jwt({ secret: config.secret}).unless({path: ['/authorization']}));
 
 // ---------------------------------------------------------
